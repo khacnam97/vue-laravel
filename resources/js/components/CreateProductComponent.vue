e762f1b0d1e318aaba013f7099086c2e384f679d
khacnam97 <31648392+khacnam97@users.noreply.github.com>
2021-04-14 16:42:14 +0700

    14/4/2021

--- resources/js/components/CreateProductComponent.vue
+++ resources/js/components/CreateProductComponent.vue
@@ -1,6 +1,33 @@
 / CreatePost.vue
+<style>
+/*input {*/
+/*    border: 1px solid silver;*/
+/*    border-radius: 4px;*/
+/*    background: white;*/
+/*    padding: 5px 10px;*/
+/*}*/
 
+/*.dirty {*/
+/*    border-color: #5A5;*/
+/*    background: #EFE;*/
+/*}*/
+
+
+.error {
+    /*border-color: red;*/
+    color: red;
+}
+.error-border ,.error-border:focus{
+    border-color: red;
+}
+
+/*.error:focus {*/
+/*    outline-color: #F99;*/
+/*}*/
+
+</style>
 <template>
+
     <div>
         <h1>Create A product</h1>
         <form @submit.prevent="addProduct">
@@ -14,9 +41,14 @@
             </div>
             <div class="row">
                 <div class="col-md-6">
-                    <div class="form-group">
-                        <label>Product Name:</label>
-                        <input type="text" class="form-control" v-model="name">
+                    <div class="form-group"  v-if="$v.name.required">
+                        <label >Product Name:</label>
+                        <input type="text" class="form-control " v-model.trim="$v.name.$model">
+                    </div>
+                    <div class="form-group"  v-else>
+                        <label >Product Name:</label>
+                        <input type="text" class="form-control error-border" v-model.trim="$v.name.$model">
+                        <div class="error" v-if="!$v.name.required">Name is required</div>
                     </div>
                 </div>
             </div>
@@ -49,9 +81,12 @@
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+const { required, maxLength, minLength } = require("vuelidate/lib/validators");
 export default {
     data(){
         return {
+            errors : [],
             FILE: null,
             name: '',
             title : '',
@@ -60,6 +95,11 @@ export default {
 
         }
     },
+    validations: {
+        name: {
+            required
+        },
+    },
     methods: {
         onImageChange(event) {
             this.img = URL.createObjectURL(event.target.files[0]);
@@ -67,7 +107,12 @@ export default {
         },
         addProduct(){
             const formData = new FormData();
-            formData.append('img',this.FILE,this.FILE.name);
+            if (this.FILE){
+                formData.append('img',this.FILE,this.FILE.name);
+            }
+            // if(!this.name){
+            //     this.errors.push('Name required');
+            // }
             formData.append('name',this.name);
             formData.append('title',this.title);
             formData.append('price',this.price);
